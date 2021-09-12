d073750250500e4e0b3434373e9acbcd4485c6b5
shubham <shubhamthakur1415@gmail.com>
2021-09-12 19:44:08 +0530

    added shubham data

---
+++ src/data/details/shubham.ts
@@ -0,0 +1,10 @@
+import { itemType } from "../type/itemType";
+
+export const shubham: itemType = {
+  name: "shubham thakur", // Add your fullname here
+  birthDate: "14/09", // Add your birthDate in the format of DD/MM
+  imageURL: "https://i.imgur.com/myMREGe.png", // Go to "https://imgur.com/upload", upload your image and paste the link here
+  gitHubURL: "https://github.com/shubhamjamwal", // Paste your GitHub profile URL here
+  linkedInURL: "", // Paste your LinkedIn profile URL here
+  twitterURL: "", //Paste your Twitter profile URL here (optional)
+};
