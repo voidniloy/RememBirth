28878987a5ad9ea7c6516edd744ecdfae7879620
4neesc0des <aneeskuar@gmail.com>
2021-09-12 19:43:20 +0530

    added Anish's data

---
+++ src/data/details/Anish.ts
@@ -0,0 +1,10 @@
+import { itemType } from "../type/itemType";
+
+export const Anish: itemType = {
+    name: "Anish Kumar",
+    birthDate: "04/06",
+    imageURL: "https://i.imgur.com/OG7pQrA.jpg",
+    gitHubURL: "https://github.com/4neesc0des",
+    linkedInURL: "https://www.linkedin.com/in/anish-kuar-44686819b/",
+    twitterURL: "https://twitter.com/Anishku48057912",
+};
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 import { itemType } from "./type/itemType";
 
 import { Niloy } from "./details/Niloy";
+import { Anish } from "./details/Anish"
 
-export const data: itemType[] = [Niloy];
+export const data: itemType[] = [Anish, Niloy];
